5df19f371ac75b1c9c81723da275ee004e10b3ed
Abraão Nascimento <abraaoan@neemu.com>
2023-10-20 15:56:21 +0100

    first commit

---
+++ Ghost-loading/Ghost_loadingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ghost_loadingApp.swift
+//  Ghost-loading
+//
+//  Created by Abraao Nascimento on 20/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Ghost_loadingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
